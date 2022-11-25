669d40fb6b92962ee60830fca5d258641e82dfd6
garhajis <118834668+garhajis@users.noreply.github.com>
2022-11-25 19:59:27 +0300

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,22 @@
+//Menu
+#include<stdio.h>
+int main (){
+    int item;
+    //menu display
+    printf("maize flour ksh 200 ");
+    printf("Sugar ksh 150");
+    printf("Cooking oil ksh 400");
+    printf("Lentils");
+    printf("Soap ksh 150");
+while(1){
+        printf("/n Enter item");
+}
+
+if(item==1){
+            printf("Maize flour");
+            }
+else if(item==3);
+printf("Cooking oil");
+return 0;
+}
+
